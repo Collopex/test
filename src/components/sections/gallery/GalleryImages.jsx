6a5196dd459a4e719d89e50873b68e57f18790bd
Cem <cemaliakar114@hotmail.com>
2023-01-16 00:04:59 +0300

    v7

--- src/components/sections/gallery/GalleryImages.jsx
+++ src/components/sections/gallery/GalleryImages.jsx
@@ -1,26 +1,7 @@
-import PhotoAlbum from "react-photo-album";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-
-const photos = [
-  { src: "images/photos/img1.jpg", width: 800, height: 533 },
-  { src: "images/photos/img2.1.jpg", width: 800, height: 533 },
-  { src: "images/photos/img2.jpg", width: 800, height: 533 },
-  { src: "images/photos/img3.jpg", width: 800, height: 533 },
-  { src: "images/photos/img3.1.jpg", width: 800, height: 533 },
-  { src: "images/photos/img4.jpg", width: 800, height: 533 },
-  { src: "images/photos/img6.jpg", width: 800, height: 533 },
-  { src: "images/photos/img7.jpg", width: 800, height: 533 },
-  { src: "images/photos/img8.jpg", width: 800, height: 533 },
-  { src: "images/photos/img10.jpg", width: 800, height: 533 },
-  { src: "images/photos/img11.jpg", width: 800, height: 533 },
-  { src: "images/photos/img16.jpg", width: 800, height: 533 },
-  { src: "images/photos/img14.jpg", width: 800, height: 533 },
-  { src: "images/photos/img13.jpg", width: 800, height: 533 },
-  { src: "images/photos/img12.jpg", width: 800, height: 533 },
-];
 
 function GalleryImages() {
   const galleryRef = useRef(null);
@@ -45,8 +26,68 @@ function GalleryImages() {
     );
   }, []);
   return (
-    <div className="gallery" ref={galleryRef}>
-      <PhotoAlbum layout="rows" photos={photos} />
+    <div className="gallery-container" ref={galleryRef}>
+      <div className="gallery-grid gallery-container">
+        <figure className="gallery-item">
+          <img src="images/photos/img1.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img2.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img3.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img4.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img5.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img6.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img7.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img8.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img9.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img10.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img11.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img12.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img13.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img14.jpg" alt="" />
+        </figure>
+
+        <figure className="gallery-item">
+          <img src="images/photos/img15.jpg" alt="" />
+        </figure>
+      </div>
     </div>
   );
 }
