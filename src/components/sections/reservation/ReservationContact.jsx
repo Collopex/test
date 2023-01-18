daf9c346d59b91377ac817711bc04d53b56b820b
Cem <cemaliakar114@hotmail.com>
2023-01-18 20:17:44 +0300

    v7

--- src/components/sections/reservation/ReservationContact.jsx
+++ src/components/sections/reservation/ReservationContact.jsx
@@ -55,7 +55,7 @@ function ReservationContactUs() {
         duration: 1.05,
         x: 0,
         opacity: 1,
-        ease: "Power2.easeInOut",
+        ease: "Power2.easeOut",
       }
     );
   }, []);
@@ -78,7 +78,7 @@ function ReservationContactUs() {
         duration: 1.05,
         x: 0,
         opacity: 1,
-        ease: "Power2.easeInOut",
+        ease: "Power2.easeOut",
       }
     );
   }, []);
